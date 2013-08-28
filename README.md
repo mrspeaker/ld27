18c8b8e897ab3469b258e8513ce9cd2a61d95098
Mr Speaker <mrspeaker@gmail.com>
2013-08-28 09:42:37 +0200

    find, but choose not to fix, green guy glitch bug

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 [Time Flies Straight](http://www.mrspeaker.net/dev/ld27): A game made in 48 hours for Ludum Dare #27 on the theme "10 seconds".
 
-I had a lot of ideas I tried to jam in at the last second. As a result, the code turned a bit... wobbly like the game. I intend to clean it up at some point. If you're reading this text, then that has not happened.
-
 ## Notes to self: move this back to Ω
 
 Ω is the library I started a couple of Ludum Dares ago and contains
--- scripts/entities/Moment.js
+++ scripts/entities/Moment.js
@@ -23,6 +23,17 @@
 
 			this._super(map);
 
+			/*
+				Intentional bug ahead:
+					Instead of "* 20" it should be "* (180 / this.dist)"
+
+				Because we don't scale by distance, the lil' green things
+				bob up and down too high in the distance, and show above and
+				below the walls. Watching people play the game, I noticed that
+				when they see the glitch, they go searching for the green guys.
+
+				I don't want to stop people searching, that's fo' sure.
+			*/
 			this.top = Math.sin(this.rnd + (Date.now() / 200)) * 20;
 
 			return !(this.remove);
